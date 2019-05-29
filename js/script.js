9f4b93d5db62ba37712847604f580783f31a7d82
Alexander <babich1994@mail..ru>
2019-05-29 03:18:21 +0300

    done

--- js/script.js
+++ js/script.js
@@ -7,7 +7,9 @@ let app = new Vue ({
         products: [],
         imgCatalog: 'https://placehold.it/150x150',
         searchText:'',
-        isVisibleCart: false
+        isVisibleCart: false,
+        filtered: [],
+        cartItems: []
     },
     methods: {
         getJSON(url) {
@@ -18,24 +20,48 @@ let app = new Vue ({
                     })
         },
         addProduct(product) {
-            console.log(product.id_product);
+            this.getJSON(`${API}/addToBasket.json`)
+            .then(data => {
+                if (data.result) {
+                    let find = this.cartItems.find(el => el.id_product === product.id_product);
+                    if(find){
+                        find.quantity++;
+                    } else {
+                        let prod = Object.assign ({quantity: 1}, product);
+                        this.cartItems.push(prod);
+                        console.log(this.cartItems);
+                        };
+                }
+            })
         },
+        filterGoods() {
+          this.filtered = this.filtered.filter(post => {
+            return post.product_name.toLowerCase().includes(this.searchText.toLowerCase());
+          });
+        },
+        remove(product) {
+            this.getJSON(`${API}/deleteFromBasket.json`)
+            .then(data => {
+                if (data.result) {
+                     if (product.quantity > 1) {
+                         product.quantity --;
+                     } else {
+                         this.cartItems.splice (this.cartItems.indexOf(product), 1)
+                     }
+                }
+            })
+        } 
     },
     mounted() {
         this.getJSON(`${API + this.catalogURL}`)
         .then (data => {
             for (el of data) {
-                this.products.push (el)
+                this.products.push (el);
+                this.filtered.push (el);
+                console.log(this.filtered);
             }
-        })
-    },
-    computed: {
-        filterGoods() {
-          return this.products.filter(post => {
-            return post.product_name.toLowerCase().includes(this.searchText.toLowerCase());
-          })
-        }
-  }
+        })  
+    }
 })
 
 
